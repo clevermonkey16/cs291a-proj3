77fc97f1e11f246491cfb638aeb2ea1cb4984e4b
Steven Jiang <stevenjiang@ucsb.edu>
2025-11-09 20:52:27 -0800

    idk man

--- config/application.rb
+++ config/application.rb
@@ -37,3 +37,16 @@ config.middleware.use ActionDispatch::Session::ActiveRecordStore, {
   same_site: Rails.env.development? ? :lax : :none,
   secure: Rails.env.production?
 }
+
+config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins [
+        'http://localhost:5173',
+        'http://127.0.0.1:5173',
+    ]
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true
+  end
+end
